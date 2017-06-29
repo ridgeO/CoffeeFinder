0fff2ebf8fd480ae245c8d0f4c052a8152efc895
Ridge <rto@MBAir.local>
2017-06-29 12:22:23 -0700

    add stack navigator and search component

--- app.js
+++ app.js
@@ -1,53 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
+import { AppRegistry } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Search from './components/Search.js';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class CoffeeFinder extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+const RootNavigator = StackNavigator(
+  {
+    Search: {name: 'Search', screen: Search }
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  { headerMode: 'screen' }
+)
 
-AppRegistry.registerComponent('CoffeeFinder', () => CoffeeFinder);
+AppRegistry.registerComponent('CoffeeFinder', () => RootNavigator);
